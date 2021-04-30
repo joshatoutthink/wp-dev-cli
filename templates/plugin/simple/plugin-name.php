7b0b555593b4e477d3b0e4c8a02522618538e0b7
Joshua Kennedy <josh@outthinkgroup.com>
2021-04-30 12:34:59 -0400

    renamed template and added a new one

---
+++ templates/plugin/simple/plugin-name.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Your Name or Your Company
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+define( 'PLUGIN_NAME_VERSION', '1.0.0' );
