7dffd8dfa3d72cf397ea19fb81d4d81e36861f16
Joshua Kennedy <josh@outthinkgroup.com>
2021-04-30 10:27:02 -0400

    removed uneeded types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,4 @@
-export type Choices<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<
-  infer Choices
->
-  ? Choices
-  : never;
-export type CreateCommandsType = [
-  "cpt",
-  "plugin",
-  "admin-page",
-  "theme",
-  "endpoint",
-  "script"
-];
-
-export type CreateCommandTypes = Choices<CreateCommandsType>;
-
-export type CreateRunnerType = Record<string, (args: any) => void>;
-
 export type OptionsType = {
   option: string;
   value: number | boolean | string;
-  //
 }[];
