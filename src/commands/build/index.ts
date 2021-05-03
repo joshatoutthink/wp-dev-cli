b92fe4c469a0e86eea734f2450c77bacf2731b78
Joshua Kennedy <josh@outthinkgroup.com>
2021-05-03 12:50:40 -0400

    cleaned up file

--- src/commands/build/index.ts
+++ src/commands/build/index.ts
@@ -3,8 +3,6 @@ import { argMerger } from "../../utils";
 import * as fs from "fs";
 import * as path from "path";
 import zipDir from "./zipDir";
-// import ncp from "ncp";
-// import rimraf from "rimraf";
 
 export default async function createZippedBuild(optionsFromYargs: OptionsType) {
   const options = argMerger(optionsFromYargs, "zip");
@@ -12,33 +10,9 @@ export default async function createZippedBuild(optionsFromYargs: OptionsType) {
   const output = options.output;
   const outputDir = path.dirname(output);
 
-  //copy to temp
-
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
   }
 
-  await zipDir(target, output);
+  await zipDir(target, output, options.ignoreFiles);
 }
-
-// function ncpPromise(inT: string, outT: string): Promise<any> {
-//   return new Promise((res, reject) => {
-//     ncp(
-//       inT,
-//       outT,
-//       {
-//         filter: (path) => {
-//           console.log(path);
-//           return !path.includes("builds") || path.includes("temp");
-//         },
-//       },
-//       (e) => {
-//         if (e) {
-//           reject(e);
-//         } else {
-//           res("pk");
-//         }
-//       }
-//     );
-//   });
-// }
